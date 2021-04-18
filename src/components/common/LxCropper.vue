6d1a54b7aad764024c4daf338681e7ebc33f4a05
18028799719 <3233as>
2021-04-19 00:17:11 +0800

    提交

--- src/components/common/LxCropper.vue
+++ src/components/common/LxCropper.vue
@@ -3,17 +3,32 @@
         <Modal :width="600" v-model="dialog" :z-index="0" title="图片裁剪" :mask-closable="false">
             <div class="avatar-layout">
                 <div class="cropper-box">
-                    <vueCropper ref="cropper" mode="contain" :img="imgUrl" :autoCrop="true" :canMove="false" :canScale="false" :fixed="true"></vueCropper>
+                    <div class="cropper-img">
+                        <VueCropper ref="cropper" :img="option.img" :autoCrop="option.autoCrop" :canMove="option.canMove" :canScale="option.canScale" :fixed="option.fixed" :fixedBox="option.fixedBox" :autoCropWidth="option.autoCropWidth" :autoCropHeight="option.autoCropHeight" @realTime="realTime"></VueCropper>
+                    </div>
+                    <div class="cropper-btn">
+                        <Button @click="realTime">放大</Button>
+                        <Button @click="realTime">缩小</Button>
+                        <Button @click="rotateLeft">左转</Button>
+                        <Button @click="rotateRight">右转</Button>
+                    </div>
                 </div>
-                <div class="cropper-box">
-                    <Upload ref="upload" action="" :before-upload="upload" :show-upload-list="false">
-                        <Button icon="ios-cloud-upload-outline">上传图片</Button>
-                    </Upload>
+                <div class="cropper-preview">
+                    <div class="show-preview" :style="{'width': '80px', 'height':'80px',  'overflow': 'hidden', 'margin': '5px'}">
+                        <div :style="previews.div" class="preview">
+                            <img :src="previews.url" :style="previews.img">
+                        </div>
+                    </div>
+                    <div class="upload-btn">
+                        <Upload ref="upload" action="" :before-upload="upload" :show-upload-list="false">
+                            <Button icon="ios-cloud-upload-outline">上传图片</Button>
+                        </Upload>
+                    </div>
                 </div>
             </div>
             <div slot="footer">
                 <Button type="text" size="large" @click="close">取消</Button>
-                <Button type="primary" size="large" @click="save">确定</Button>
+                <Button type="text" size="large" @click="save">确定</Button>
             </div>
         </Modal>
     </div>
@@ -26,12 +41,31 @@ export default {
         return {
             dialog: false,
             uploadFile: null,
-            imgUrl: null,
+            //剪切图片上传
+            previews: {},
+            // 选项设置
+            option: {
+                img: null,
+                // 是否默认生成截图框
+                autoCrop: true,
+                // 移动
+                canMove: false,
+                // 缩放
+                canScale: true,
+                // 是否开启截图框宽高固定比例
+                fixed: true,
+                // 固定截图框大小不能改变
+                fixedBox: true,
+                // 默认生成截图框的宽度
+                autoCropWidth: 80,
+                // 默认生成截图框的度
+                autoCropHeight: 80,
+            },
             limit: {
                 size: 512,
                 format: ["jpg", "jpeg", "png"],
-                img: null
-            }
+                img: null,
+            },
         };
     },
     methods: {
@@ -50,7 +84,7 @@ export default {
             let fileReader = new FileReader();
             fileReader.readAsDataURL(file);
             fileReader.onload = () => {
-                this.imgUrl = fileReader.result;
+                this.option.img = fileReader.result;
             };
             return false;
         },
@@ -61,43 +95,70 @@ export default {
             return file.size > this.limit.size * 1024;
         },
         close() {
-            this.imgUrl = null;
+            this.option.img = null;
             this.dialog = false;
-            this.$emit('close');
+            this.$emit("close");
         },
         save() {
             if (!this.uploadFile) {
                 this.$Message.info("请上传图片");
                 return;
             }
-            this.$refs.cropper.getCropBlob(data => {
-                this.$emit('confirm', data);
+            this.$refs.cropper.getCropBlob((data) => {
+                this.$emit("confirm", data);
                 this.dialog = false;
             });
-        }
-    }
+        },
+        // 左旋转
+        rotateLeft() {
+            this.$refs.cropper.rotateLeft();
+        },
+        // 右旋转
+        rotateRight() {
+            this.$refs.cropper.rotateRight();
+        },
+        // 实时预览
+        realTime(data) {
+            this.previews = data;
+        },
+    },
 };
 </script>
 
 <style scoped>
-.user-center {
-    width: 100%;
-    height: 100%;
-}
-.userAvatar-box {
-    width: 100%;
-    margin-left: 80px;
-    margin-bottom: 20px;
-}
-.base-info {
-    width: 400px;
-}
 .avatar-layout {
+    height: 300px;
     display: flex;
     justify-content: space-around;
 }
 .cropper-box {
-    width: 250px;
+    width: 300px;
+    height: 300px;
+}
+.cropper-img {
+    width: 300px;
     height: 250px;
+}
+.cropper-btn {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+}
+.cropper-preview {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    width: 300px;
+    height: 300px;
+}
+.cropper-preview .preview {
+    overflow: hidden;
+    width: 80px;
+    height: 80px;
+    border: 1px solid red;
+}
+.upload-btn {
+    margin-top: 20px;
 }
 </style>
