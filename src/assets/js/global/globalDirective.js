82d3c27bdc6c10a0f482c159882affca728747c1
18028799719 <342127943@qq.com>
2022-02-23 23:28:20 +0800

    自定义指令

--- src/assets/js/global/globalDirective.js
+++ src/assets/js/global/globalDirective.js
@@ -6,13 +6,24 @@
  * @LastEditTime  2022-02-23 17:16:47
  */
 import Vue from "vue";
+import globalConsts from "@/assets/js/global/globalConsts";
+import { Message } from "view-design";
 
 /**权限指令**/
 Vue.directive("permission", {
     inserted: function (el, binding, vnode) {
-        var hasPermission = vnode.context.$store.state.user.loginInfo.lsAuthString.indexOf(binding.value) > -1;
+        var loginInfo = vnode.context.$store.state.user.loginInfo;
+        var hasPermission = loginInfo.lsAuthString.indexOf(binding.value) > -1;
         if (!hasPermission) {
-            el.parentNode.removeChild(el);
+            var permissionProcessType = loginInfo.systemCustomData.permissionProcessType;
+            if (permissionProcessType === globalConsts.permissionProcessType.hide) {
+                el.parentNode.removeChild(el);
+            } else if (permissionProcessType === globalConsts.permissionProcessType.show) {
+                el.addEventListener("click", function (event) {
+                    Message.warning("权限不足");
+                    return false;
+                });
+            }
         }
     }
 });
